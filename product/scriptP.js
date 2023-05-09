5b66c9bb05467f0e0c044a87caa66d91097b8af3
Aubrey Moya-Mendez Trotter <Aubrey@AdminisatorsMBP.lan>
2023-05-08 21:12:30 -0400

    Completed product and contact pages

--- product/scriptP.js
+++ product/scriptP.js
@@ -1,3 +1,14 @@
+const stylesSubsection = document.getElementById("styles-subsection");
+const styleTitle = document.getElementsByClassName("style-main-title")[0];
+
+styleTitle.addEventListener("click", () => {
+  stylesSubsection.style.display = "block";
+});
+
+stylesSubsection.addEventListener("click", () => {
+  stylesSubsection.style.display = "none";
+});
+
 fetch("products.json")
   .then((response) => response.json())
   .then((data) => {
@@ -26,10 +37,18 @@ fetch("products.json")
       overlay.appendChild(description);
 
       const price = document.createElement("p");
+      price.classList.add("price");
       price.textContent = product.price;
       div.appendChild(price);
 
       products.appendChild(div);
+
+      const button = document.createElement("a");
+      button.classList.add("order-button");
+      button.setAttribute("href", "https://example.com");
+      button.textContent = "Add to Order";
+
+      div.appendChild(button);
 
       // Add mouseover event listener to the image
       image.addEventListener("mouseover", () => {
@@ -42,37 +61,12 @@ fetch("products.json")
         overlay.style.opacity = "0";
         image.style.filter = "brightness(1)";
       });
+
+      image.addEventListener("click", () => {
+        overlay.style.opacity = "1";
+        image.style.filter = "brightness(0.3)";
+      });
     });
   });
 
-// fetch("products.json")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const products = document.getElementById("products");
-
-//     data.forEach((product) => {
-//       const div = document.createElement("div");
-//       div.classList.add("product");
-
-//       const name = document.createElement("h2");
-//       name.textContent = product.name;
-//       div.appendChild(name);
-
-//       const image = document.createElement("img");
-//       image.src = product.image;
-//       image.alt = product.name;
-//       div.appendChild(image);
-
-//       const description = document.createElement("p");
-//       description.textContent = product.description;
-//       div.appendChild(description);
-
-//       const price = document.createElement("p");
-//       price.textContent = product.price;
-//       div.appendChild(price);
-
-//       products.appendChild(div);
-//     });
-//   });
-
-// const productImage = document.querySelector("img");
+// If time allows, edit and add back for on click of styles dropdown
